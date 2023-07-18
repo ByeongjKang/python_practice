5498d9f2dd5ce898e2c209971472466f0de9ef05
ByeongjKang <138206586+ByeongjKang@users.noreply.github.com>
2023-07-18 18:27:37 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,12 +1,24 @@
 def solution(k, score):
+    result = []
+
+    for i in range(1, len(score)+1) :
+        temp = score[0:i]
+        temp.sort(reverse=True)
+        if i < k+1 : 
+            result.append(temp[-1])
+        else :
+            result.append(temp[k-1])
+    return result
+
+
+"""def solution(k, score):
     if len(score) < k :
         result = [ min(score[:i]) for i in range(1,len(score)+1) ]
         return result
     result = [ min(score[:i]) for i in range(1, k+1)]
     temp = score[:k]
-
     for j in range(k,len(score)) :
         temp.append(score[j])
         temp.remove(min(temp))
         result.append(min(temp))
-    return result
+    return result"""
