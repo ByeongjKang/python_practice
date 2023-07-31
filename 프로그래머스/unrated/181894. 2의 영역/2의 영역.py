6c2625d5802d0f392b352f734b99b7dfe4bd09d7
ByeongjKang <138206586+ByeongjKang@users.noreply.github.com>
2023-07-31 17:08:52 +0900

    [unrated] Title: 2의 영역, Time: 3.73 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -0,0 +1,8 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)) :
+        if arr[i] == 2 :
+            answer.append(i)
+    if len(answer) == 0 : 
+        return [-1]
+    return arr[min(answer):max(answer)+1]
